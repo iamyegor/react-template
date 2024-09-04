d094e331555b1b98e80f6a4d3be50adbe11274d9
yegor <astery227@gmail.com>
2024-09-04 18:16:42 +0300

    feat: add tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+            screens: {
+                xs: "400px",
+                sm: "600px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+            },
+        },
+        container: {
+            center: true,
+            padding: "20px",
+            screens: {
+                sm: "1240px",
+                md: "1240px",
+                lg: "1240px",
+                xl: "1240px",
+                "2xl": "1700px",
+            },
+        },
     },
     plugins: [],
 };
